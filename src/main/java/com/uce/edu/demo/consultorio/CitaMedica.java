2a20ca463b941062a2969eac011bb4d70bac6b69
Juan Jimenez <jijimenez@uce.edu.ec>
2022-05-27 20:18:29 -0500

    Segundo cambio

--- src/main/java/com/uce/edu/demo/consultorio/CitaMedica.java
+++ src/main/java/com/uce/edu/demo/consultorio/CitaMedica.java
@@ -2,4 +2,8 @@ package com.uce.edu.demo.consultorio;
 
 public class CitaMedica {
 
+	private Doctor doctor;
+	private Paciente paciente;
+	
+	
 }
--- src/main/java/com/uce/edu/demo/consultorio/Paciente.java
+++ src/main/java/com/uce/edu/demo/consultorio/Paciente.java
@@ -1,5 +1,24 @@
 package com.uce.edu.demo.consultorio;
 
 public class Paciente {
+	
+	private String nombre;
+	private int edad;
+	
+	//GET Y SET
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 
 }
