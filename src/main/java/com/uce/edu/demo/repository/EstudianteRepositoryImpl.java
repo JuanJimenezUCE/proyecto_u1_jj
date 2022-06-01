9047caf455f3737396c21bf4d592ccff4eadd2b1
Juan Jimenez <jijimenez@uce.edu.ec>
2022-05-31 19:54:21 -0500

    estereotipos service y repository

---
+++ src/main/java/com/uce/edu/demo/repository/EstudianteRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.uce.edu.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.demo.modelo.Estudiante;
+
+
+@Repository
+public class EstudianteRepositoryImpl implements IEstudianteRepository{
+
+	@Override
+	public void insertar(Estudiante e) {
+		// TODO Auto-generated method stub
+		//Aqui se realiza la insercion en la base de datos
+		System.out.println("Se ha insertado en la base de datos del estudiante" +e);
+	}
+
+	@Override
+	public Estudiante buscar(String apellido) {
+		// TODO Auto-generated method stub
+		System.out.println("Se ha buscado en la base de datos del estudiante" +apellido);
+		return null;
+	}
+
+	@Override
+	public void actualizar(Estudiante e) {
+		// TODO Auto-generated method stub
+		System.out.println("Se ha actualizado en la base de datos del estudiante" +e);
+	}
+
+	@Override
+	public void eliminar(String cedula) {
+		// TODO Auto-generated method stub
+		System.out.println("Se ha eliminado de la base de datos del estudiante"+cedula);
+	}
+
+}
