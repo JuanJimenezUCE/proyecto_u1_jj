4b6eefa0968f2fafc014dbcff2702d4a37e7de11
Juan Jimenez <jijimenez@uce.edu.ec>
2022-06-17 20:12:19 -0500

    Lookup en Spring Framework

--- src/main/java/com/uce/edu/demo/ProyectoU1JjApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1JjApplication.java
@@ -1,43 +1,36 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.concesionario.modelo.Auto;
-import com.uce.edu.demo.concesionario.modelo.GerenteVentas;
-import com.uce.edu.demo.concesionario.modelo.Vendedor;
-import com.uce.edu.demo.concesionario.modelo.VentaAuto;
-import com.uce.edu.demo.concesionario.service.IAutoService;
-import com.uce.edu.demo.concesionario.service.IVentaAutoService;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.modelo.Materia;
+import com.uce.edu.demo.modelo.Matricula;
 import com.uce.edu.demo.modelo.ProfesorGeneral;
 import com.uce.edu.demo.modelo.ProfesorMateria;
 import com.uce.edu.demo.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1JjApplication  implements CommandLineRunner{
+	@Autowired
+	private ProfesorGeneral general;
 	
 	@Autowired
-	private Vendedor vendedor1;
+	private ProfesorGeneral general1;
 	
 	@Autowired
-	private Vendedor vendedor2;
-	@Autowired
-	private Vendedor vendedor3;
-	@Autowired
-	private GerenteVentas gerente;
+	private ProfesorMateria materia;
 	
 	@Autowired
-	private GerenteVentas gerente1;
+	private ProfesorMateria materia1;
 	
 	@Autowired
-	private IVentaAutoService iVentaAutoService;
+	private IMatriculaService iMatriculaService;
 	
-	@Autowired
-	private IAutoService iAutoService;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1JjApplication.class, args);
 	}
@@ -45,55 +38,37 @@ public class ProyectoU1JjApplication  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-	
 		System.out.println("EJEMPLO SINGLETON");
+		this.general.setNombre("Edison");
+		this.general.setApellido("Cayambe");
 		
-		this.gerente.setNombre("Juan ");
-		this.gerente.setApellido("Jimenez");
-		System.out.println(this.gerente);
+		System.out.println(this.general);
+		System.out.println("----");
+		System.out.println(this.general1);
+		
+		this.general1.setNombre("Pepito");
+		System.out.println("----");
+		System.out.println(this.general);
+		
 		
 		System.out.println("----");
-		System.out.println(this.gerente1);
-		
-		this.gerente1.setNombre("Luis");
-		System.out.println("----");
-		System.out.println(this.gerente);
-		
-		
-		System.out.println("----");
-		System.out.println(this.gerente1);
-		
+		System.out.println(this.general1);
 		
 		System.out.println("EJEMPLO PROTOTYPE");
+		this.materia.setNombre("Juan");
+		this.materia.setApellido("Jimenez");
+		System.out.println(this.materia);
 		
-		this.vendedor1.setNombre("James");
-		this.vendedor1.setApellido("Hetfield");
-		this.vendedor1.setnVentas("3");
-		
-		System.out.println(this.vendedor1);
-		this.vendedor2.setNombre("Fernando");
-		this.vendedor2.setApellido("Malla");
-		this.vendedor2.setnVentas("5");
-		
-		System.out.println(this.vendedor2);
 		System.out.println("-----");
-		System.out.println(this.vendedor3);
-		System.out.println("-----");
-		Auto a = new Auto();
-		a.setMarca("KIA");
-		a.setModelo("Rio");
-		a.setProcedencia("Mexico");
-		a.setFechaFabricacion(LocalDateTime.of(2020, 10, 15, 16, 30));
-		this.iAutoService.ingresarAuto(a);
-		VentaAuto v= new VentaAuto();
-		v.setNumero("1");
-		v.setAuto(a);
-		v.setFecha(LocalDateTime.now());
-		this.iVentaAutoService.ingresarVenta(v);
+		System.out.println(this.materia1);
 		
-		this.vendedor1.setnVentas("4");
-		System.out.println(this.vendedor1);
+		System.out.println("-----");
+        
+		Matricula matricula1=new Matricula();
+		matricula1.setEstudiante(new Estudiante());
+		matricula1.setMateria(new ArrayList<Materia>());
+		matricula1.setNumero("1234");
+		this.iMatriculaService.ingresarMatricula(matricula1);
 	}
 
 }
--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Matricula;
@@ -20,11 +21,32 @@ public class MatriculaServiceImpl implements IMatriculaService{
 	@Autowired
 	private ProfesorMateria profesorMateria;
 	
+	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		
+		
+		return null;
+	}
+	
+	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		
+		ProfesorMateria profeM = new ProfesorMateria();
+		profeM.setApellido("Tapia");
+		profeM.setNombre("Jose");
+		return profeM;
+	}
 	@Override
 	public void ingresarMatricula(Matricula m) {
 		// TODO Auto-generated method stub
+		
+		
 		System.out.println("DI desde Service SINGLETON "+this.profesorGeneral);
 		System.out.println("DI desde Service PROTOTYPE "+this.profesorMateria);
+		
+		System.out.println("DI desde un metodo: "+this.obtenerProfesorG());
+		
+		System.out.println("DI desde un metodo: "+this.obtenerProfesorM());
 		this.matriRepository.insertar(m);
 	}
 
